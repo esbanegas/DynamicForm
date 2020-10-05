2c2ad0f056aaa5ab0db1ef17917519c8cae7031c
Denis Tercero <dtercero@caracolknits.com>
2020-10-04 23:30:55 -0600

    add new columns

---
+++ backend/Migrations/20201005052759_AddColumnDescription.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class AddColumnDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "Forms",
+                type: "varchar(200)",
+                unicode: false,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "dbo",
+                table: "Forms");
+        }
+    }
+}
